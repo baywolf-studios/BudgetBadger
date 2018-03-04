72a9b2bc867e23a8df551c392f7426b787660e57
baywolf-studios <34951520+baywolf-studios@users.noreply.github.com>
2018-03-04 16:52:34 -0600

    net worth report

--- BudgetBadger.Core/Logic/IReportLogic.cs
+++ BudgetBadger.Core/Logic/IReportLogic.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using BudgetBadger.Models;
 
 namespace BudgetBadger.Core.Logic
 {
     public interface IReportLogic
     {
-        Task<Dictionary<string, decimal>> GetNetWorthReport();
+        Task<Result<IEnumerable<ReportDataPoint>>> GetNetWorthReport();
     }
 }
--- BudgetBadger.Logic/ReportLogic.cs
+++ BudgetBadger.Logic/ReportLogic.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using BudgetBadger.Core.DataAccess;
 using BudgetBadger.Core.Logic;
+using BudgetBadger.Models;
 
 namespace BudgetBadger.Logic
 {
@@ -25,19 +26,48 @@ namespace BudgetBadger.Logic
             _envelopeDataAccess = envelopeDataAccess;
         }
 
-        public async Task<Dictionary<string, decimal>> GetNetWorthReport()
+        public async Task<Result<IEnumerable<ReportDataPoint>>> GetNetWorthReport()
         {
-            var result = new Dictionary<string, decimal>();
+            var result = new Result<IEnumerable<ReportDataPoint>>();
+            var dataPoints = new List<ReportDataPoint>();
 
-            var transactions = await _transactionDataAccess.ReadTransactionsAsync();
-            var activeTransactions = transactions.Where(t => t.IsActive);
-            var months = activeTransactions.Select(d => new DateTime(d.ServiceDate.Year, d.ServiceDate.Month, 1)).Distinct();
-
-            foreach (var month in months)
+            try
             {
-                var monthTransactions = activeTransactions.Where(t => t.ServiceDate.Month <= month.Month && t.ServiceDate.Year <= month.Year);
-                var monthTotal = monthTransactions.Sum(t => t.Amount);
-                result.Add(month.ToString("Y"), monthTotal);
+                var transactions = await _transactionDataAccess.ReadTransactionsAsync();
+                var activeTransactions = transactions.Where(t => t.IsActive);
+                var months = activeTransactions.Select(d => new DateTime(d.ServiceDate.Year, d.ServiceDate.Month, 1)).Distinct();
+
+                var earliestMonth = activeTransactions.Min(t => t.ServiceDate);
+                var latestMonth = activeTransactions.Max(t => t.ServiceDate);
+
+                var startMonth = new DateTime(earliestMonth.Year, earliestMonth.Month, 1).AddMonths(1).AddTicks(-1);
+                var endMonth = new DateTime(latestMonth.Year, latestMonth.Month, 1).AddMonths(1).AddTicks(-1);
+
+                int count = 0;
+                //something on these lines 
+                while (startMonth <= endMonth)
+                {
+                    var monthTransactions = activeTransactions.Where(t => t.ServiceDate <= startMonth);
+                    var monthTotal = monthTransactions.Sum(t => t.Amount);
+                    var dataPoint = new ReportDataPoint
+                    {
+                        X = count,
+                        XLabel = startMonth.ToString("Y"),
+                        Y = Convert.ToDouble(monthTotal),
+                        YLabel = monthTotal.ToString("C")
+                    };
+                    dataPoints.Add(dataPoint);
+                    count++;
+                    startMonth = startMonth.AddMonths(1);
+                }
+
+                result.Data = dataPoints;
+                result.Success = true;
+            }
+            catch (Exception ex)
+            {
+                result.Success = false;
+                result.Message = ex.Message;
             }
 
             return result;
