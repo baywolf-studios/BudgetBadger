13e43a33e7938f69f2db71135fd9f5294e0e7ff8
baywolf-studios <34951520+baywolf-studios@users.noreply.github.com>
2018-03-25 08:23:24 -0500

    prevent editing of starting balance

--- BudgetBadger.Logic/PayeeLogic.cs
+++ BudgetBadger.Logic/PayeeLogic.cs
@@ -107,7 +107,7 @@ namespace BudgetBadger.Logic
                 var allPayees = await _payeeDataAccess.ReadPayeesAsync();
 
                 var payees = allPayees.Where(p =>
-                                             p.Id != Constants.StartingBalancePayee.Id
+                                             !p.IsStartingBalance()
                                              && p.IsActive);
 
                 var tasks = payees.Select(p => GetPopulatedPayee(p));
@@ -134,7 +134,7 @@ namespace BudgetBadger.Logic
 
                 // ugly hardcoded to remove the starting balance payee.
                 // may move to a "Payee Group" type setup
-                var payees = allPayees.Where(p => p.IsActive && p.Id != Constants.StartingBalancePayee.Id)
+                var payees = allPayees.Where(p => p.IsActive && !p.IsStartingBalance())
                                       .ToList();
 
                 var includeDeleted = false; //will get this from settings dataaccess
