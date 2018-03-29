eeda80562fef988633be7e601691d774845fcc41
baywolf-studios <34951520+baywolf-studios@users.noreply.github.com>
2018-03-29 07:25:06 -0500

    bug fixes and refactoring

--- BudgetBadger.Forms/UserControls/TextEntry.xaml.cs
+++ BudgetBadger.Forms/UserControls/TextEntry.xaml.cs
@@ -96,26 +96,119 @@ namespace BudgetBadger.Forms.UserControls
             MainEntry.BindingContext = this;
             MainEntry.Focused += async (s, a) =>
             {
-                if (string.IsNullOrEmpty(MainEntry.Text))
-                await Task.WhenAll(
-                    BottomBorder.ColorTo(Color.FromRgba(0, 0, 0, .56), ActiveColor, c => BottomBorder.BackgroundColor = c, 230, Easing.CubicOut),
-                    PlaceholderLabel.TranslateTo(HiddenLabel.X - MainEntry.X, HiddenLabel.Y - MainEntry.Y, 230, Easing.CubicOut),
-                    PlaceholderLabel.ColorTo(Color.FromRgba(0,0,0,.56), ActiveColor, c => PlaceholderLabel.TextColor = c, 230, Easing.CubicOut),
-                    PlaceholderLabel.FontSizeTo(MainEntry.FontSize, HiddenLabel.FontSize, f => PlaceholderLabel.FontSize = f, 230, Easing.CubicOut)
-                );
+                await SetFocusedState();
             };
             MainEntry.Unfocused += async (s, a) =>
             {
                 if (string.IsNullOrEmpty(MainEntry.Text))
                 {
-                    await Task.WhenAll(
-                        BottomBorder.ColorTo(ActiveColor, Color.FromRgba(0, 0, 0, .56), c => BottomBorder.BackgroundColor = c, 230, Easing.CubicIn),
-                        PlaceholderLabel.TranslateTo(0, 0, 230, Easing.CubicIn),
-                        PlaceholderLabel.ColorTo(ActiveColor, Color.FromRgba(0, 0, 0, .56), c => PlaceholderLabel.TextColor = c, 230, Easing.CubicIn),
-                        PlaceholderLabel.FontSizeTo(HiddenLabel.FontSize, MainEntry.FontSize, f => PlaceholderLabel.FontSize = f, 230, Easing.CubicIn)
-                    );
+                    await SetIdleEmptyState();
+                }
+                else
+                {
+                    await SetIdleFilledState();
                 }
             };
+            MainEntry.TextChanged += async (sender, e) => 
+            {
+                if (MainEntry.IsFocused)
+                {
+                    await SetFocusedState();
+                }
+                else if (string.IsNullOrEmpty(MainEntry.Text))
+                {
+                    await SetIdleEmptyState();
+                }
+                else if (!string.IsNullOrEmpty(MainEntry.Text))
+                {
+                    await SetIdleFilledState();
+                }
+            };
+        }
+
+        async Task SetIdleEmptyState()
+        {
+            var tasks = new List<Task>();
+
+            if (BottomBorder.BackgroundColor != Color.FromRgba(0, 0, 0, .56))
+            {
+                tasks.Add(BottomBorder.ColorTo(BottomBorder.BackgroundColor, Color.FromRgba(0, 0, 0, .56), c => BottomBorder.BackgroundColor = c, 230, Easing.CubicIn));
+            }
+
+            if (PlaceholderLabel.TranslationX != 0 || PlaceholderLabel.TranslationY != 0)
+            {
+                tasks.Add(PlaceholderLabel.TranslateTo(0, 0, 230, Easing.CubicIn));
+            }
+
+            if (PlaceholderLabel.TextColor != Color.FromRgba(0,0,0,.56))
+            {
+                tasks.Add(PlaceholderLabel.ColorTo(PlaceholderLabel.TextColor, Color.FromRgba(0, 0, 0, .56), c => PlaceholderLabel.TextColor = c, 230, Easing.CubicIn));
+            }
+
+            if (PlaceholderLabel.FontSize != MainEntry.FontSize)
+            {
+                tasks.Add(PlaceholderLabel.FontSizeTo(PlaceholderLabel.FontSize, MainEntry.FontSize, f => PlaceholderLabel.FontSize = f, 230, Easing.CubicIn));
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        async Task SetIdleFilledState()
+        {
+            var tasks = new List<Task>();
+
+            if (BottomBorder.BackgroundColor != Color.FromRgba(0, 0, 0, .56))
+            {
+                tasks.Add(BottomBorder.ColorTo(BottomBorder.BackgroundColor, Color.FromRgba(0, 0, 0, .56), c => BottomBorder.BackgroundColor = c, 230, Easing.CubicIn));
+            }
+
+            var placholderTranslateX = HiddenLabel.X - MainEntry.X;
+            var placeholderTranslateY = HiddenLabel.Y - MainEntry.Y;
+            if (PlaceholderLabel.TranslationX != placholderTranslateX || PlaceholderLabel.TranslationY != placeholderTranslateY)
+            {
+                tasks.Add(PlaceholderLabel.TranslateTo(placholderTranslateX, placeholderTranslateY, 230, Easing.CubicIn));
+            }
+
+            if (PlaceholderLabel.TextColor != Color.FromRgba(0, 0, 0, .56))
+            {
+                tasks.Add(PlaceholderLabel.ColorTo(PlaceholderLabel.TextColor, Color.FromRgba(0, 0, 0, .56), c => PlaceholderLabel.TextColor = c, 230, Easing.CubicIn));
+            }
+
+            if (PlaceholderLabel.FontSize != HiddenLabel.FontSize)
+            {
+                tasks.Add(PlaceholderLabel.FontSizeTo(PlaceholderLabel.FontSize, HiddenLabel.FontSize, f => PlaceholderLabel.FontSize = f, 230, Easing.CubicIn));
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        async Task SetFocusedState()
+        {
+            var tasks = new List<Task>();
+
+            if (BottomBorder.BackgroundColor != ActiveColor)
+            {
+                tasks.Add(BottomBorder.ColorTo(BottomBorder.BackgroundColor, ActiveColor, c => BottomBorder.BackgroundColor = c, 230, Easing.CubicIn));
+            }
+
+            var placholderTranslateX = HiddenLabel.X - MainEntry.X;
+            var placeholderTranslateY = HiddenLabel.Y - MainEntry.Y;
+            if (PlaceholderLabel.TranslationX != placholderTranslateX || PlaceholderLabel.TranslationY != placeholderTranslateY)
+            {
+                tasks.Add(PlaceholderLabel.TranslateTo(placholderTranslateX, placeholderTranslateY, 230, Easing.CubicIn));
+            }
+
+            if (PlaceholderLabel.TextColor != ActiveColor)
+            {
+                tasks.Add(PlaceholderLabel.ColorTo(PlaceholderLabel.TextColor, ActiveColor, c => PlaceholderLabel.TextColor = c, 230, Easing.CubicIn));
+            }
+
+            if (PlaceholderLabel.FontSize != HiddenLabel.FontSize)
+            {
+                tasks.Add(PlaceholderLabel.FontSizeTo(PlaceholderLabel.FontSize, HiddenLabel.FontSize, f => PlaceholderLabel.FontSize = f, 230, Easing.CubicIn));
+            }
+
+            await Task.WhenAll(tasks);
         }
     }
 }
