b42126b81bc2d322a5dbee471f2e601847ee634e
baywolf-studios <34951520+baywolf-studios@users.noreply.github.com>
2018-03-29 19:01:54 -0500

    refactoring

---
+++ BudgetBadger.macOS/Effects/BorderlessEntryEffect.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AppKit;
+using BudgetBadger.macOS.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+[assembly: ResolutionGroupName("Wolf")]
+[assembly: ExportEffect(typeof(BorderlessEntryEffect), "BorderlessEntryEffect")]
+namespace BudgetBadger.macOS.Effects
+{
+    public class BorderlessEntryEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            if (Control is NSTextField entry)
+            {
+                entry.Layer.BorderWidth = 0;
+                entry.BackgroundColor = NSColor.Clear;
+                entry.Bordered = false;
+                entry.FocusRingType = NSFocusRingType.None;
+            }
+        }
+
+        protected override void OnDetached()
+        {
+        }
+    }
+}
