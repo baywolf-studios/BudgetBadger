4d4dc525125c91c37e24e6e9d0479d9fedcbf936
baywolf-studios <34951520+baywolf-studios@users.noreply.github.com>
2018-03-29 19:37:50 -0500

    fixes on controls

---
+++ BudgetBadger.iOS/Effects/BorderlessEditorEffect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BudgetBadger.iOS.Effects;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportEffect(typeof(BorderlessEditorEffect), "BorderlessEditorEffect")]
+namespace BudgetBadger.iOS.Effects
+{
+    public class BorderlessEditorEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            if (Control is UITextView entry)
+            {
+                entry.Layer.BorderWidth = 0;
+                //entry.BorderStyle = UITextBorderStyle.None;
+            }
+        }
+
+        protected override void OnDetached()
+        {
+        }
+    }
+}
