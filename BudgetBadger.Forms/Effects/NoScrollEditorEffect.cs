556f256311fee3a1f99ddc39450d4fa6a10657ea
baywolf-studios <34951520+baywolf-studios@users.noreply.github.com>
2018-03-30 19:03:27 -0500

    fixes

---
+++ BudgetBadger.Forms/Effects/NoScrollEditorEffect.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace BudgetBadger.Forms.Effects
+{
+    public class NoScrollEditorEffect : RoutingEffect
+    {
+        public NoScrollEditorEffect() : base("Wolf.NoScrollEditorEffect")
+        {
+        }
+    }
+}
---
+++ BudgetBadger.iOS/Effects/NoScrollEditorEffect.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using BudgetBadger.iOS.Effects;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportEffect(typeof(NoScrollEditorEffect), "NoScrollEditorEffect")]
+namespace BudgetBadger.iOS.Effects
+{
+    public class NoScrollEditorEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            if (Control is UITextView control)
+            {
+                control.ScrollEnabled = false;
+            }
+        }
+
+        protected override void OnDetached()
+        {
+        }
+    }
+}
