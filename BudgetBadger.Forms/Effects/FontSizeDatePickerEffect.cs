d469652a21944c00d08150002fd441cef9f0650e
baywolf-studios <34951520+baywolf-studios@users.noreply.github.com>
2018-04-02 20:30:14 -0500

    custom controls

---
+++ BudgetBadger.Forms/Effects/FontSizeDatePickerEffect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace BudgetBadger.Forms.Effects
+{
+    public class FontSizeDatePickerEffect : RoutingEffect
+    {
+        public double FontSize { get; set; }
+
+        public FontSizeDatePickerEffect() : base("Wolf.FontSizeDatePickerEffect")
+        {
+        }
+    }
+}
