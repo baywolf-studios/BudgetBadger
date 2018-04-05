8482c7c2c7e565f37bf93949b656c55904a6cb29
baywolf-studios <34951520+baywolf-studios@users.noreply.github.com>
2018-04-05 16:42:22 -0500

    control updates

---
+++ BudgetBadger.Droid/Effects/BorderlessPickerEffect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Android.Views;
+using Android.Views.InputMethods;
+using Android.Widget;
+using BudgetBadger.Droid.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportEffect(typeof(BorderlessPickerEffect), "BorderlessPickerEffect")]
+namespace BudgetBadger.Droid.Effects
+{
+    public class BorderlessPickerEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            if (Control is EditText picker)
+            {
+                picker.SetBackground(null);
+
+                var layoutParams = new ViewGroup.MarginLayoutParams(Control.LayoutParameters);
+                layoutParams.SetMargins(0, 0, 0, 0);
+                picker.LayoutParameters = layoutParams;
+                picker.SetPadding(0, 0, 0, 0);
+                picker.ImeOptions = (ImeAction)ImeFlags.NoExtractUi;
+            }
+
+        }
+
+        protected override void OnDetached()
+        {
+        }
+    }
+}
+
---
+++ BudgetBadger.Forms/Effects/BorderlessPickerEffect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace BudgetBadger.Forms.Effects
+{
+    public class BorderlessPickerEffect : RoutingEffect
+    {
+        public BorderlessPickerEffect() : base("Wolf.BorderlessPickerEffect")
+        {
+        }
+    }
+}
+
