4221853a86ab6b86500e1206f363e5100c3049e2
baywolf-studios <34951520+baywolf-studios@users.noreply.github.com>
2018-04-07 11:50:30 -0500

    prefix option

--- BudgetBadger.Forms/Animation/AnimationExtensions.cs
+++ BudgetBadger.Forms/Animation/AnimationExtensions.cs
@@ -30,5 +30,20 @@ namespace BudgetBadger.Forms.Animation
             self.Animate(animationName, transform, callback, 16, length, easing, (v, c) => taskCompletionSource.SetResult(c));
             return taskCompletionSource.Task;
         }
+
+        public static Task<bool> ThicknessTo(this VisualElement self, Thickness fromColor, Thickness toColor, Action<Thickness> callback, uint length = 250, Easing easing = null, string animationName = "ThicknessTo")
+        {
+            Thickness transform(double t) => new Thickness(fromColor.Left + t * (toColor.Left - fromColor.Left),
+                                                           fromColor.Top + t * (toColor.Top - fromColor.Top),
+                                                           fromColor.Right + t * (toColor.Right - fromColor.Right),
+                                                           fromColor.Bottom + t * (toColor.Bottom - fromColor.Bottom));
+            
+            easing = easing ?? Easing.Linear;
+
+            var taskCompletionSource = new TaskCompletionSource<bool>();
+
+            self.Animate(animationName, transform, callback, 16, length, easing, (v, c) => taskCompletionSource.SetResult(c));
+            return taskCompletionSource.Task;
+        }
     }
 }
