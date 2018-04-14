bde01507c3f8e9f5fd516c761acfcfaccfc077da
baywolf-studios <34951520+baywolf-studios@users.noreply.github.com>
2018-04-13 21:52:24 -0500

    header

---
+++ BudgetBadger.Forms/UserControls/SearchHeader.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using BudgetBadger.Forms.Animation;
+using Xamarin.Forms;
+
+namespace BudgetBadger.Forms.UserControls
+{
+    public partial class SearchHeader : AbsoluteLayout
+    {
+        uint _animationLength = 150;
+
+        public static BindableProperty SearchTextProperty = BindableProperty.Create(nameof(SearchText), typeof(string), typeof(SearchHeader), defaultBindingMode: BindingMode.TwoWay);
+        public string SearchText
+        {
+            get => (string)GetValue(SearchTextProperty);
+            set => SetValue(SearchTextProperty, value);
+        }
+
+        public static BindableProperty SearchCommandProperty = BindableProperty.Create(nameof(SearchCommand), typeof(ICommand), typeof(SearchHeader), defaultBindingMode: BindingMode.TwoWay);
+        public ICommand SearchCommand
+        {
+            get => (ICommand)GetValue(SearchCommandProperty);
+            set => SetValue(SearchCommandProperty, value);
+        }
+
+        async void SearchTapped(object sender, EventArgs e)
+        {
+            if (SearchBoxFrame.TranslationX > 0) //currently hidden
+            {
+                //SearchBoxFrame.InputTransparent = false;
+                //show it
+                await SearchBoxFrame.TranslateTo(0, 0, _animationLength, Easing.CubicOut);
+                EntryControl.Focus();
+            }
+            else //currently showing
+            {
+                //SearchBoxFrame.InputTransparent = true;
+
+                SearchText = string.Empty;
+
+                //hide it
+                await SearchBoxFrame.TranslateTo(SearchBoxFrame.Width, 0, _animationLength, Easing.CubicOut);
+            }
+        }
+
+        public SearchHeader()
+        {
+            InitializeComponent();
+
+            EntryControl.BindingContext = this;
+
+            EntryControl.TextChanged += (sender, e) =>
+            {
+                if (e.OldTextValue != e.NewTextValue)
+                {
+                    if (SearchCommand?.CanExecute(SearchText) != false)
+                    {
+                        SearchCommand?.Execute(SearchText);
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ BudgetBadger.Forms/UserControls/TestControl.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace BudgetBadger.Forms.UserControls
+{
+    public partial class TestControl : ContentView
+    {
+        public TestControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
