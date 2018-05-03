5e3428bb8f781a2377b5d43c74062a72d83d3f30
baywolf-studios <34951520+baywolf-studios@users.noreply.github.com>
2018-05-02 19:44:48 -0500

    ui updates

--- BudgetBadger.Forms/Transactions/TransactionSelectionPage.xaml.cs
+++ BudgetBadger.Forms/Transactions/TransactionSelectionPage.xaml.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using BudgetBadger.Forms.UserControls;
 using Xamarin.Forms;
 
 namespace BudgetBadger.Forms.Transactions
 {
-    public partial class TransactionSelectionPage : ContentPage
+    public partial class TransactionSelectionPage : ChildSearchPage
     {
         public TransactionSelectionPage()
         {
