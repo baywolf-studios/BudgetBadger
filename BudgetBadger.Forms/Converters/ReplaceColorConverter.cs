6a553072b6a17bbd3e31611948c04dec52313369
baywolf-studios <34951520+baywolf-studios@users.noreply.github.com>
2018-07-31 21:02:41 -0500

    more updates to master detail view for desktop

---
+++ BudgetBadger.Forms/Converters/ReplaceColorConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace BudgetBadger.Forms.Converters
+{
+    public class ReplaceColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                var PreferredColor = (Color)value;
+                var ReplaceMap = new Dictionary<string, string>
+                {
+                    { "currentColor", PreferredColor.GetHexString() }
+                };
+
+                return ReplaceMap;
+            }
+
+            return "currentColor";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BudgetBadger.Forms/Views/MainDesktopPage.xaml.cs
+++ BudgetBadger.Forms/Views/MainDesktopPage.xaml.cs
@@ -7,9 +7,22 @@ namespace BudgetBadger.Forms.Views
 {
     public partial class MainDesktopPage : MasterDetailPage
     {
+        void Handle_Tapped(object sender, EventArgs e)
+        {
+            EnvelopesFrame.BackgroundColor = Color.White;
+            AccountsFrame.BackgroundColor = Color.White;
+            PayeesFrame.BackgroundColor = Color.White;
+            ReportsFrame.BackgroundColor = Color.White;
+            SettingsFrame.BackgroundColor = Color.White;
+
+            var frame = (Frame)sender;
+            frame.BackgroundColor = (Color)Application.Current.Resources["SelectedNavigationItemColor"];
+        }
+
         public MainDesktopPage()
         {
             InitializeComponent();
+            EnvelopesFrame.BackgroundColor = (Color)Application.Current.Resources["SelectedNavigationItemColor"];
         }
     }
 }
