b226ff2165f7d6a611432528b5dd38a0bf9113df
baywolf-studios <34951520+baywolf-studios@users.noreply.github.com>
2018-08-01 07:34:05 -0500

    updates

--- BudgetBadger.Forms/Views/MainDesktopPage.xaml.cs
+++ BudgetBadger.Forms/Views/MainDesktopPage.xaml.cs
@@ -1,28 +1,87 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Linq;
+using FFImageLoading.Svg.Forms;
 using Xamarin.Forms;
 
 namespace BudgetBadger.Forms.Views
 {
     public partial class MainDesktopPage : MasterDetailPage
     {
+        Color _backgroundColor
+        {
+            get => (Color)Application.Current.Resources["BackgroundColor"];
+        }
+
+        Color _secondaryTextColor
+        {
+            get => (Color)Application.Current.Resources["SecondaryTextColor"];
+        }
+
+        Color _primaryColor
+        {
+            get => (Color)Application.Current.Resources["PrimaryColor"];
+        }
+
+        string _replaceColorMap;
+
+        void SetAllInactive()
+        {
+            EnvelopesFrame.BackgroundColor = _backgroundColor;
+            AccountsFrame.BackgroundColor = _backgroundColor;
+            PayeesFrame.BackgroundColor = _backgroundColor;
+            ReportsFrame.BackgroundColor = _backgroundColor;
+            SettingsFrame.BackgroundColor = _backgroundColor;
+
+            EnvelopesIcon.ReplaceStringMap.Clear();
+            EnvelopesIcon.ReplaceStringMap.Add(_replaceColorMap, _secondaryTextColor.GetHexString());
+            EnvelopesIcon.ReloadImage();
+            EnvelopesLabel.TextColor = _secondaryTextColor;
+
+            AccountsIcon.ReplaceStringMap.Clear();
+            AccountsIcon.ReplaceStringMap.Add(_replaceColorMap, _secondaryTextColor.GetHexString());
+            AccountsIcon.ReloadImage();
+            AccountsLabel.TextColor = _secondaryTextColor;
+
+            PayeesIcon.ReplaceStringMap.Clear();
+            PayeesIcon.ReplaceStringMap.Add(_replaceColorMap, _secondaryTextColor.GetHexString());
+            PayeesIcon.ReloadImage();
+            PayeesLabel.TextColor = _secondaryTextColor;
+
+            ReportsIcon.ReplaceStringMap.Clear();
+            ReportsIcon.ReplaceStringMap.Add(_replaceColorMap, _secondaryTextColor.GetHexString());
+            ReportsIcon.ReloadImage();
+            ReportsLabel.TextColor = _secondaryTextColor;
+
+            SettingsIcon.ReplaceStringMap.Clear();
+            SettingsIcon.ReplaceStringMap.Add(_replaceColorMap, _secondaryTextColor.GetHexString());
+            SettingsIcon.ReloadImage();
+            SettingsLabel.TextColor = _secondaryTextColor;
+        }
+
         void Handle_Tapped(object sender, EventArgs e)
         {
-            EnvelopesFrame.BackgroundColor = Color.White;
-            AccountsFrame.BackgroundColor = Color.White;
-            PayeesFrame.BackgroundColor = Color.White;
-            ReportsFrame.BackgroundColor = Color.White;
-            SettingsFrame.BackgroundColor = Color.White;
+            SetAllInactive();
 
             var frame = (Frame)sender;
             frame.BackgroundColor = (Color)Application.Current.Resources["SelectedNavigationItemColor"];
+
+            var stackLayout = (StackLayout)frame.Content;
+
+            var currentIcon = (SvgCachedImage)stackLayout.Children.FirstOrDefault(c => c is SvgCachedImage);
+            currentIcon.ReplaceStringMap.Clear();
+            currentIcon.ReplaceStringMap.Add(_replaceColorMap, _primaryColor.GetHexString());
+            currentIcon.ReloadImage();
+
+            var currentLabel = (Label)stackLayout.Children.FirstOrDefault(c => c is Label);
+            currentLabel.TextColor = _primaryColor;
         }
 
         public MainDesktopPage()
         {
             InitializeComponent();
-            EnvelopesFrame.BackgroundColor = (Color)Application.Current.Resources["SelectedNavigationItemColor"];
+
+            _replaceColorMap = EnvelopesIcon.ReplaceStringMap.FirstOrDefault().Key;
         }
     }
 }
