c973b316962678e2ffdad3e681dac2e656d583e4
baywolf-studios <34951520+baywolf-studios@users.noreply.github.com>
2018-08-30 07:36:52 -0700

    more upw changes

---
+++ BudgetBadger.UWP/Effects/BorderlessDatePickerEffect.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using BudgetBadger.UWP.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportEffect(typeof(BorderlessDatePickerEffect), "BorderlessDatePickerEffect")]
+namespace BudgetBadger.UWP.Effects
+{
+    public class BorderlessDatePickerEffect : PlatformEffect
+    {
+        public BorderlessDatePickerEffect()
+        {
+        }
+
+        protected override void OnAttached()
+        {
+            if (Control is Windows.UI.Xaml.Controls.DatePicker control)
+            {
+
+                control.BorderThickness = new Windows.UI.Xaml.Thickness(0);
+
+                control.Margin = new Windows.UI.Xaml.Thickness(0);
+
+                control.Padding = new Windows.UI.Xaml.Thickness(0);
+
+            }
+        }
+
+        protected override void OnDetached()
+        {
+        }
+    }
+}
