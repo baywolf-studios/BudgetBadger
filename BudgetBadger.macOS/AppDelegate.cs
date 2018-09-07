bf05c12ee0c77bc2ce26a33eb8a86b6bd749616c
baywolf-studios <matthewpritchett@Matthews-Mac-Pro.local>
2018-09-06 20:28:54 -0500

    macos

--- BudgetBadger.macOS/AppDelegate.cs
+++ BudgetBadger.macOS/AppDelegate.cs
@@ -1,18 +1,44 @@
 ﻿using AppKit;
+using BudgetBadger.Forms;
+using FFImageLoading.Forms.Platform;
+using FFImageLoading.Svg.Forms;
 using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
 
 namespace BudgetBadger.macOS
 {
     [Register("AppDelegate")]
     public class AppDelegate : NSApplicationDelegate
     {
+        NSWindow window;
+
         public AppDelegate()
         {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+            var rect = new CoreGraphics.CGRect(200, 1000, 1024, 768);
+            window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+            window.Title = "Xamarin.Forms on Mac!"; // choose your own Title here
+            window.TitleVisibility = NSWindowTitleVisibility.Hidden;
+        }
+
+        public override NSWindow MainWindow
+        {
+            get { return window; }
         }
 
         public override void DidFinishLaunching(NSNotification notification)
         {
-            // Insert code here to initialize your application
+            Xamarin.Forms.Forms.Init();
+            SimpleAuth.NativeSafariAuthenticator.Activate();
+            Syncfusion.SfDataGrid.XForms.MacOS.SfDataGridRenderer.Init();
+            Syncfusion.ListView.XForms.MacOS.SfListViewRenderer.Init();
+            CachedImageRenderer.Init();
+            var ignore = typeof(SvgCachedImage);
+
+            LoadApplication(new App());
+            base.DidFinishLaunching(notification);
         }
 
         public override void WillTerminate(NSNotification notification)
--- BudgetBadger.macOS/Main.cs
+++ BudgetBadger.macOS/Main.cs
@@ -7,6 +7,7 @@ namespace BudgetBadger.macOS
         static void Main(string[] args)
         {
             NSApplication.Init();
+            NSApplication.SharedApplication.Delegate = new AppDelegate(); // add this line
             NSApplication.Main(args);
         }
     }
