95d9734e77ea5aa5dcb32e8c3101976041eb0b88
baywolf-studios <matthewpritchett@Matthews-Mac-Pro.local>
2019-04-10 05:02:48 -0500

    macos button fix

---
+++ BudgetBadger.Forms/UserControls/Button2.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace BudgetBadger.Forms.UserControls
+{
+    public partial class Button2 : Button
+    {
+        double originalWidth = -1;
+
+        public Button2()
+        {
+            InitializeComponent();
+
+            PropertyChanged += (sender, e) =>
+            {
+                if (e.PropertyName == nameof(Width) && Device.RuntimePlatform == Device.macOS && originalWidth < Width)
+                {
+                    WidthRequest = Width + Padding.HorizontalThickness;
+                    originalWidth = WidthRequest;
+                }
+            };
+        }
+    }
+}
