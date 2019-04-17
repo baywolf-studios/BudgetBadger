352046ff8eb69a8b8aa4ccf1ccbdaf906987e960
baywolf-studios <34951520+baywolf-studios@users.noreply.github.com>
2019-04-17 03:55:02 -0700

    nuget and sort scroll changes

---
+++ BudgetBadger.Forms/Comparers/PayeeSorter.cs
@@ -0,0 +1,23 @@
+﻿using BudgetBadger.Models;
+using Syncfusion.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BudgetBadger.Forms.Comparers
+{
+    public class PayeeSorter : IComparer<Object>, ISortDirection
+    {
+        public ListSortDirection SortDirection { get; set; }
+
+        public int Compare(object x, object y)
+        {
+            if (x is Payee payee)
+            {
+                return payee.CompareTo(y);
+            }
+
+            return 0;
+        }
+    }
+}
