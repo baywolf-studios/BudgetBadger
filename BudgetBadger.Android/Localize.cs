3e861bada39201c679f39122e6e585d035c30c59
baywolf-studios <matthewpritchett@Matthews-Mac-Pro.local>
2019-04-20 09:37:19 -0500

    android localize

---
+++ BudgetBadger.Android/Localize.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+using BudgetBadger.Core.LocalizedResources;
+using BudgetBadger.Droid;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Localize))]
+namespace BudgetBadger.Droid
+{
+    public class Localize : ILocalize
+    {
+        static CultureInfo _currentCulture;
+
+        public CultureInfo GetLocale()
+        {
+            return _currentCulture;
+        }
+
+        public void SetLocale(CultureInfo cultureInfo)
+        {
+            _currentCulture = cultureInfo;
+            CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+            CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+            CultureInfo.CurrentCulture = cultureInfo;
+            CultureInfo.CurrentUICulture = cultureInfo;
+        }
+
+        public CultureInfo GetDeviceCultureInfo()
+        {
+            var netLanguage = "en";
+            var prefLanguageOnly = "en";
+
+            var androidLocale = Java.Util.Locale.Default;
+            var pref = androidLocale.ToString();
+            if (!String.IsNullOrEmpty(pref))
+            {
+                prefLanguageOnly = pref.Substring(0, 2);
+                netLanguage = pref.Replace("_", "-");
+            }
+
+            CultureInfo cultureInfo;
+            try
+            {
+                cultureInfo = new CultureInfo(netLanguage);
+            }
+            catch
+            {
+                // Fallback to first two characters, e.g. "en"
+                cultureInfo = new CultureInfo(prefLanguageOnly);
+            }
+
+            return cultureInfo;
+        }
+    }
+}
