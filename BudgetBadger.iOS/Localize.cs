d11cff839e391f5ce22e67785fe92ca9f1b1f22e
baywolf-studios <matthewpritchett@Matthews-Mac-Pro.local>
2019-04-20 10:22:30 -0500

    localization

--- BudgetBadger.iOS/Localize.cs
+++ BudgetBadger.iOS/Localize.cs
@@ -30,36 +30,71 @@ namespace BudgetBadger.iOS
         public CultureInfo GetDeviceCultureInfo()
         {
             var netLanguage = "en";
-            var prefLanguageOnly = "en";
-
             if (NSLocale.PreferredLanguages.Length > 0)
             {
                 var pref = NSLocale.PreferredLanguages[0];
-                prefLanguageOnly = pref.Substring(0, 2);
-
-                if (prefLanguageOnly == "pt")
-                {
-                    if (pref == "pt")
-                        pref = "pt-BR"; // Brazilian
-                    else
-                        pref = "pt-PT"; // Portuguese
-                }
-
-                netLanguage = pref.Replace("_", "-");
+                netLanguage = iOSToDotnetLanguage(pref);
             }
-
-            CultureInfo cultureInfo;
+            // this gets called a lot - try/catch can be expensive so consider caching or something
+            System.Globalization.CultureInfo ci = null;
             try
             {
-                cultureInfo = new CultureInfo(netLanguage);
+                ci = new System.Globalization.CultureInfo(netLanguage);
             }
-            catch
+            catch (CultureNotFoundException e1)
             {
-                // Fallback to first two characters, e.g. "en"
-                cultureInfo = new CultureInfo(prefLanguageOnly);
+                // iOS locale not valid .NET culture (eg. "en-ES" : English in Spain)
+                // fallback to first characters, in this case "en"
+                try
+                {
+                    var fallback = ToDotnetFallbackLanguage(new PlatformCulture(netLanguage));
+                    ci = new System.Globalization.CultureInfo(fallback);
+                }
+                catch (CultureNotFoundException e2)
+                {
+                    // iOS language not valid .NET culture, falling back to English
+                    ci = new System.Globalization.CultureInfo("en");
+                }
             }
+            return ci;
+        }
 
-            return cultureInfo;
+        string iOSToDotnetLanguage(string iOSLanguage)
+        {
+            // .NET cultures don't support underscores
+            string netLanguage = iOSLanguage.Replace("_", "-");
+
+            //certain languages need to be converted to CultureInfo equivalent
+            switch (iOSLanguage)
+            {
+                case "ms-MY":   // "Malaysian (Malaysia)" not supported .NET culture
+                case "ms-SG":    // "Malaysian (Singapore)" not supported .NET culture
+                    netLanguage = "ms"; // closest supported
+                    break;
+                case "gsw-CH":  // "Schwiizertüütsch (Swiss German)" not supported .NET culture
+                    netLanguage = "de-CH"; // closest supported
+                    break;
+                    // add more application-specific cases here (if required)
+                    // ONLY use cultures that have been tested and known to work
+            }
+            return netLanguage;
+        }
+
+        string ToDotnetFallbackLanguage(PlatformCulture platCulture)
+        {
+            var netLanguage = platCulture.LanguageCode; // use the first part of the identifier (two chars, usually);
+            switch (platCulture.LanguageCode)
+            {
+                case "pt":
+                    netLanguage = "pt-PT"; // fallback to Portuguese (Portugal)
+                    break;
+                case "gsw":
+                    netLanguage = "de-CH"; // equivalent to German (Switzerland) for this app
+                    break;
+                    // add more application-specific cases here (if required)
+                    // ONLY use cultures that have been tested and known to work
+            }
+            return netLanguage;
         }
     }
 }
