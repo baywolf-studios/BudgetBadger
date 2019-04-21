f29524f45254edc9ffad2696bc9aba229b0c624d
baywolf-studios <matthewpritchett@Matthews-Mac-Pro.local>
2019-04-21 14:31:24 -0500

    card control

---
+++ BudgetBadger.iOS/Renderers/CardRenderer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+using BudgetBadger.Forms.UserControls;
+using BudgetBadger.iOS.Renderers;
+using CoreGraphics;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Card), typeof(CardRenderer))]
+namespace BudgetBadger.iOS.Renderers
+{
+    public class CardRenderer : FrameRenderer 
+    {
+        private Card _card;
+
+        public static void Initialize()
+        {
+            // empty, but used for beating the linker
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Frame> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e?.NewElement != null && e?.NewElement is Card)
+            {
+                _card = (Card)e.NewElement;
+                this.Elevate(_card.Elevation);
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (e?.PropertyName == nameof(Card.Elevation) || e?.PropertyName == nameof(Card.BackgroundColor))
+            {
+                this.Elevate(_card.Elevation);
+            }
+        }
+    }
+}
