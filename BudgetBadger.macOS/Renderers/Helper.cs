9ba0a7b741775b595dccb02b64010a3970eb1559
baywolf-studios <matthewpritchett@Matthews-Mac-Pro.local>
2019-04-22 04:50:02 -0500

    more material style shadows

--- BudgetBadger.macOS/Renderers/Helper.cs
+++ BudgetBadger.macOS/Renderers/Helper.cs
@@ -11,9 +11,15 @@ namespace BudgetBadger.macOS.Renderers
             view.Shadow = new NSShadow();
             view.Layer.MasksToBounds = false;
             view.Layer.ShadowColor = NSColor.Black.CGColor;
-            view.Layer.ShadowOffset = new CGSize(0, (nfloat)(-1 * elevation));
-            view.Layer.ShadowOpacity = 0.24f;
-            view.Layer.ShadowRadius = Math.Abs(elevation);
+
+            var offset = elevation < 10 ? Math.Floor(elevation / 2) + 1 : elevation - 4;
+            view.Layer.ShadowOffset = new CGSize(0, (nfloat)(-1 * offset));
+
+            var shadowOpacity = (float)(24 - Math.Round(elevation / 10)) / 100;
+            view.Layer.ShadowOpacity = shadowOpacity;
+
+            var blurRadius = elevation == 1 ? 3 : elevation * 2;
+            view.Layer.ShadowRadius = Math.Abs(blurRadius);
         }
     }
 }
