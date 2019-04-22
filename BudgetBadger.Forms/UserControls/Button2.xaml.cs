76260e0c2e4eb7f01b51264d641d1db40991f03a
baywolf-studios <matthewpritchett@Matthews-Mac-Pro.local>
2019-04-22 08:06:13 -0500

    button updates

--- BudgetBadger.Forms/UserControls/Button2.xaml.cs
+++ BudgetBadger.Forms/UserControls/Button2.xaml.cs
@@ -7,10 +7,26 @@ namespace BudgetBadger.Forms.UserControls
     public partial class Button2 : Button
     {
         double originalWidth = -1;
+        Color backgroundColor;
+
+        public static BindableProperty ElevationProperty = BindableProperty.Create(nameof(Elevation), typeof(float), typeof(Button2), 0.0f);
+        public float Elevation
+        {
+            get
+            {
+                return (float)GetValue(ElevationProperty);
+            }
+            set
+            {
+                SetValue(ElevationProperty, value);
+            }
+        }
 
         public Button2()
         {
             InitializeComponent();
+
+            backgroundColor = BackgroundColor;
 
             PropertyChanged += (sender, e) =>
             {
@@ -18,6 +34,40 @@ namespace BudgetBadger.Forms.UserControls
                 {
                     WidthRequest = Width + Padding.HorizontalThickness;
                     originalWidth = WidthRequest;
+                }
+            };
+
+            Pressed += (sender, e) =>
+            {
+                backgroundColor = BackgroundColor;
+
+                if (backgroundColor != Color.Transparent)
+                {
+                    Elevation += 6;
+                }
+
+                if (Device.RuntimePlatform == Device.macOS || Device.RuntimePlatform == Device.iOS)
+                {
+                    if (backgroundColor == Color.Transparent)
+                    {
+                        BackgroundColor = (Color)Application.Current.Resources["SecondaryButtonActiveColor"];
+                    }
+                    else
+                    {
+                        BackgroundColor = Color.FromHsla(backgroundColor.Hue, backgroundColor.Saturation, 0.9 * backgroundColor.Luminosity, backgroundColor.A);
+                    }
+                }
+            };
+            Released += (sender, e) =>
+            {
+                if (Device.RuntimePlatform == Device.macOS || Device.RuntimePlatform == Device.iOS)
+                {
+                    BackgroundColor = backgroundColor;
+                }
+
+                if (backgroundColor != Color.Transparent)
+                {
+                    Elevation -= 6;
                 }
             };
         }
