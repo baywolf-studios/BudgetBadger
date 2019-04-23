90ec46cc084d34b7e63aa2cfd2f88dd8530a0ee4
baywolf-studios <matthewpritchett@Matthews-Mac-Pro.local>
2019-04-23 04:22:53 -0500

    updated button

--- BudgetBadger.Forms/UserControls/Button2.xaml.cs
+++ BudgetBadger.Forms/UserControls/Button2.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using BudgetBadger.Forms.Animation;
 using Xamarin.Forms;
 
 namespace BudgetBadger.Forms.UserControls
@@ -7,44 +10,40 @@ namespace BudgetBadger.Forms.UserControls
     public partial class Button2 : Button
     {
         double originalWidth = -1;
-        Color backgroundColor;
-        Color pressedBackgroundColor;
-        float elevation;
-        float pressedElevation;
 
-        public static BindableProperty IsRaisedProperty = BindableProperty.Create(nameof(IsRaised), typeof(bool), typeof(Button2), false);
-        public bool IsRaised
+        public static BindableProperty RestingBackgroundColorProperty = BindableProperty.Create(nameof(RestingBackgroundColor), typeof(Color), typeof(Button2), Color.Accent);
+        public Color RestingBackgroundColor
         {
-            get
-            {
-                return (bool)GetValue(IsRaisedProperty);
-            }
-            set
-            {
-                SetValue(IsRaisedProperty, value);
-            }
+            get => (Color)GetValue(RestingBackgroundColorProperty);
+            set => SetValue(RestingBackgroundColorProperty, value);
         }
 
-        public static BindableProperty ElevationProperty = BindableProperty.Create(nameof(Elevation), typeof(float), typeof(Button2), 0.0f);
-        public float Elevation
+        public static BindableProperty RestingBorderColorProperty = BindableProperty.Create(nameof(RestingBorderColor), typeof(Color), typeof(Button2), Color.Accent);
+        public Color RestingBorderColor
         {
-            get
-            {
-                return (float)GetValue(ElevationProperty);
-            }
-            set
-            {
-                SetValue(ElevationProperty, value);
-            }
+            get => (Color)GetValue(RestingBorderColorProperty);
+            set => SetValue(RestingBorderColorProperty, value);
+        }
+
+        public static BindableProperty ActiveBackgroundColorProperty = BindableProperty.Create(nameof(ActiveBackgroundColor), typeof(Color), typeof(Button2), Color.Accent);
+        public Color ActiveBackgroundColor
+        {
+            get => (Color)GetValue(ActiveBackgroundColorProperty);
+            set => SetValue(ActiveBackgroundColorProperty, value);
+        }
+
+        public static BindableProperty ActiveBorderColorProperty = BindableProperty.Create(nameof(ActiveBorderColor), typeof(Color), typeof(Button2), Color.Accent);
+        public Color ActiveBorderColor
+        {
+            get => (Color)GetValue(ActiveBorderColorProperty);
+            set => SetValue(ActiveBorderColorProperty, value);
         }
 
         public Button2()
         {
             InitializeComponent();
 
-            backgroundColor = BackgroundColor;
-            pressedBackgroundColor = Color.FromHsla(backgroundColor.Hue, backgroundColor.Saturation, 0.9 * backgroundColor.Luminosity, backgroundColor.A);
-            pressedElevation = Elevation;
+            UpdateResting();
 
             PropertyChanged += (sender, e) =>
             {
@@ -53,45 +52,49 @@ namespace BudgetBadger.Forms.UserControls
                     WidthRequest = Width + Padding.HorizontalThickness;
                     originalWidth = WidthRequest;
                 }
+
+                if (e.PropertyName == nameof(RestingBackgroundColor)
+                    || e.PropertyName == nameof(RestingBorderColor)
+                    ||e.PropertyName == nameof(ActiveBackgroundColor)
+                    || e.PropertyName == nameof(ActiveBorderColor)
+                    || e.PropertyName == nameof(IsPressed))
+                {
+                    if (IsPressed)
+                    {
+                        UpdateActive();
+                    }
+                    else
+                    {
+                        UpdateResting();
+                    }
+                }
             };
 
             Pressed += (sender, e) =>
             {
-                if (BackgroundColor != pressedBackgroundColor)
-                {
-                    backgroundColor = BackgroundColor;
-
-                    if (IsRaised)
-                    {
-                        pressedBackgroundColor = Color.FromHsla(backgroundColor.Hue, backgroundColor.Saturation, 0.9 * backgroundColor.Luminosity, backgroundColor.A);
-
-                    }
-                    else
-                    {
-                        pressedBackgroundColor = (Color)Application.Current.Resources["SecondaryButtonActiveColor"];
-                    }
-
-                    BackgroundColor = pressedBackgroundColor;
-                }
-
-                if (IsRaised && Elevation != pressedElevation)
-                {
-                    elevation = Elevation;
-                    pressedElevation = Elevation + 6;
-                    Elevation = pressedElevation;
-                }
+                UpdateActive();
             };
 
             Released += (sender, e) =>
             {
-                UpdateReleased();
+                UpdateResting();
             };
         }
 
-        public void UpdateReleased()
+        void UpdateColors(Color backgroundColor, Color borderColor)
         {
             BackgroundColor = backgroundColor;
-            Elevation = elevation;
+            BorderColor = borderColor;
+        }
+
+        public void UpdateResting()
+        {
+            UpdateColors(RestingBackgroundColor, RestingBorderColor);
+        }
+
+        public void UpdateActive()
+        {
+            UpdateColors(ActiveBackgroundColor, ActiveBorderColor);
         }
     }
 }
