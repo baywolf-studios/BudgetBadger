3773278eacdd956815599601033cd9a7098ee79d
baywolf-studios <matthewpritchett@Matthews-Mac-Pro.local>
2019-04-23 04:24:07 -0500

    updated renderers

--- BudgetBadger.Android/Renderers/Button2Renderer.cs
+++ BudgetBadger.Android/Renderers/Button2Renderer.cs
@@ -17,41 +17,9 @@ namespace BudgetBadger.Droid.Renderers
         {
         }
 
-        private Button2 _card;
-
         public static void Initialize()
         {
             // empty, but used for beating the linker
-        }
-
-        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
-        {
-            base.OnElementChanged(e);
-
-            if (e?.NewElement != null && e?.NewElement is Button2)
-            {
-                _card = (Button2)e.NewElement;
-                UpdateElevation();
-            }
-        }
-
-        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            base.OnElementPropertyChanged(sender, e);
-
-            if (e?.PropertyName == nameof(Card.Elevation) || e?.PropertyName == VisualElement.BackgroundColorProperty.PropertyName)
-            {
-                UpdateElevation();
-            }
-        }
-
-        private void UpdateElevation()
-        {
-            StateListAnimator = null;
-
-            // set the elevation manually
-            ViewCompat.SetElevation(this, _card.Elevation);
-            ViewCompat.SetElevation(Control, _card.Elevation);
         }
     }
 }
