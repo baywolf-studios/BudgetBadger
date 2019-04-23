60c0fe3e7c449e7e910117c15f369815b14c00ae
baywolf-studios <34951520+baywolf-studios@users.noreply.github.com>
2019-04-23 04:49:57 -0500

    fixing uwp button style

--- BudgetBadger.UWP/Renderers/Button2Renderer.cs
+++ BudgetBadger.UWP/Renderers/Button2Renderer.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.UWP;
 
@@ -30,14 +31,14 @@ namespace BudgetBadger.UWP.Renderers
                 this.Control.AddHandler(PointerReleasedEvent, new PointerEventHandler(Control_Released), true);
                 this.Control.AddHandler(PointerCanceledEvent, new PointerEventHandler(Control_Released), true);
                 this.Control.AddHandler(PointerReleasedEvent, new PointerEventHandler(Control_Released), true);
-               
+
                 _card = (Button2)e.NewElement;
             }
         }
 
         void Control_Released(object sender, PointerRoutedEventArgs e)
         {
-            _card?.UpdateReleased();
+            _card?.UpdateResting();
         }
     }
 }
