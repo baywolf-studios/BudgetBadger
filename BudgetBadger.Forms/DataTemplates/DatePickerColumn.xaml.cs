d15b8b4f317431a82965706c845309d9784a23e1
baywolf-studios <matthewpritchett@Matthews-Mac-Pro.local>
2019-06-08 20:41:54 -0500

    datepicker and picker columns

---
+++ BudgetBadger.Forms/DataTemplates/DatePickerColumn.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace BudgetBadger.Forms.DataTemplates
+{
+    public partial class DatePickerColumn : ContentView
+    {
+        public static BindableProperty DateProperty =
+            BindableProperty.Create(nameof(Date),
+                                    typeof(DateTime),
+                                    typeof(DatePickerColumn),
+                                    defaultValue: DateTime.Now,
+                                    defaultBindingMode: BindingMode.TwoWay);
+        public DateTime Date
+        {
+            get => (DateTime)GetValue(DateProperty);
+            set => SetValue(DateProperty, value);
+        }
+
+        public static BindableProperty SaveCommandProperty = BindableProperty.Create(nameof(SaveCommand), typeof(ICommand), typeof(DatePickerColumn));
+        public ICommand SaveCommand
+        {
+            get => (ICommand)GetValue(SaveCommandProperty);
+            set => SetValue(SaveCommandProperty, value);
+        }
+
+        public static BindableProperty SaveCommandParameterProperty = BindableProperty.Create(nameof(SaveCommandParameter), typeof(object), typeof(DatePickerColumn));
+        public object SaveCommandParameter
+        {
+            get => GetValue(SaveCommandParameterProperty);
+            set => SetValue(SaveCommandParameterProperty, value);
+        }
+
+        public DatePickerColumn()
+        {
+            InitializeComponent();
+            DateControl.BindingContext = this;
+
+            PropertyChanged += (sender, e) =>
+            {
+                if (e.PropertyName == nameof(IsEnabled))
+                {
+                    DateControl.IsEnabled = IsEnabled;
+                }
+            };
+
+            DateControl.Unfocused += (sender, e) =>
+            {
+                BackgroundColor = Color.Transparent;
+
+                if (SaveCommand != null && SaveCommand.CanExecute(SaveCommandParameter))
+                {
+                    SaveCommand.Execute(SaveCommandParameter);
+                }
+            };
+
+            DateControl.Focused += (sender, e) =>
+            {
+                BackgroundColor = (Color)Application.Current.Resources["SelectedItemColor"];
+            };
+        }
+
+        void Handle_Tapped(object sender, System.EventArgs e)
+        {
+            if (!DateControl.IsFocused)
+            {
+                DateControl.Focus();
+            }
+        }
+    }
+}
---
+++ BudgetBadger.Forms/DataTemplates/PickerColumn.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace BudgetBadger.Forms.DataTemplates
+{
+    public partial class PickerColumn : ContentView
+    {
+        public BindingBase ItemDisplayBinding
+        {
+            get => PickerControl.ItemDisplayBinding;
+            set => PickerControl.ItemDisplayBinding = value;
+        }
+
+        public static BindableProperty SelectedIndexProperty = BindableProperty.Create(nameof(SelectedIndex), typeof(int), typeof(PickerColumn), -1, BindingMode.TwoWay);
+        public int SelectedIndex
+        {
+            get => (int)GetValue(SelectedIndexProperty);
+            set => SetValue(SelectedIndexProperty, value);
+        }
+
+        public static readonly BindableProperty SelectedItemProperty = BindableProperty.Create(nameof(SelectedItem), typeof(object), typeof(PickerColumn), null, BindingMode.TwoWay);
+        public object SelectedItem
+        {
+            get => GetValue(SelectedItemProperty);
+            set => SetValue(SelectedItemProperty, value);
+        }
+
+        public static BindableProperty ItemsSourceProperty = BindableProperty.Create(nameof(ItemsSource), typeof(IList), typeof(PickerColumn), null, BindingMode.TwoWay);
+        public IList ItemsSource
+        {
+            get => (IList)GetValue(ItemsSourceProperty);
+            set => SetValue(ItemsSourceProperty, value);
+        }
+
+        public static BindableProperty SaveCommandProperty = BindableProperty.Create(nameof(SaveCommand), typeof(ICommand), typeof(PickerColumn));
+        public ICommand SaveCommand
+        {
+            get => (ICommand)GetValue(SaveCommandProperty);
+            set => SetValue(SaveCommandProperty, value);
+        }
+
+        public static BindableProperty SaveCommandParameterProperty = BindableProperty.Create(nameof(SaveCommandParameter), typeof(object), typeof(PickerColumn));
+        public object SaveCommandParameter
+        {
+            get => GetValue(SaveCommandParameterProperty);
+            set => SetValue(SaveCommandParameterProperty, value);
+        }
+
+        public PickerColumn()
+        {
+            InitializeComponent();
+            PickerControl.BindingContext = this;
+
+            PropertyChanged += (sender, e) =>
+            {
+                if (e.PropertyName == nameof(IsEnabled))
+                {
+                    PickerControl.IsEnabled = IsEnabled;
+                }
+            };
+
+            PickerControl.Unfocused += (sender, e) =>
+            {
+                BackgroundColor = Color.Transparent;
+
+                if (SaveCommand != null && SaveCommand.CanExecute(SaveCommandParameter))
+                {
+                    SaveCommand.Execute(SaveCommandParameter);
+                }
+            };
+
+            PickerControl.Focused += (sender, e) =>
+            {
+                BackgroundColor = (Color)Application.Current.Resources["SelectedItemColor"];
+            };
+        }
+
+        void Handle_Tapped(object sender, System.EventArgs e)
+        {
+            if (!PickerControl.IsFocused)
+            {
+                PickerControl.Focus();
+            }
+        }
+    }
+}
