2f118df7efacc42cfec87e86b1bdd20d9be5841b
Matthew Pritchett <matthewpritchett@Pritchett-iMac.local>
2019-11-14 12:48:11 -0600

    more testing stuff

---
+++ BudgetBadger.Tests/TestModels/TestTransactions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using BudgetBadger.Models;
+
+namespace BudgetBadger.Tests.TestModels
+{
+    public static class TestTransactions
+    {
+        public static readonly Transaction NewTransaction = new Transaction();
+
+
+    }
+}
