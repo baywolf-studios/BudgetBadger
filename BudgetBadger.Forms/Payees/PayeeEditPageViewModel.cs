bd93cd96596771fae2ba24354c4edd8cb11f2578
Matthew Pritchett <matthewpritchett@Pritchett-iMac.local>
2019-11-30 15:37:14 -0600

    added delete confirmation

--- BudgetBadger.Forms/Payees/PayeeEditPageViewModel.cs
+++ BudgetBadger.Forms/Payees/PayeeEditPageViewModel.cs
@@ -145,33 +145,41 @@ namespace BudgetBadger.Forms.Payees
                 return;
             }
 
-            IsBusy = true;
+            var confirm = await _dialogService.DisplayAlertAsync(_resourceContainer.GetResourceString("AlertConfirmation"),
+                _resourceContainer.GetResourceString("AlertConfirmDelete"),
+                _resourceContainer.GetResourceString("AlertOk"),
+                _resourceContainer.GetResourceString("AlertCancel"));
 
-            try
+            if (confirm)
             {
-                BusyText = _resourceContainer.GetResourceString("BusyTextDeleting");
-                var result = await _payeeLogic.SoftDeletePayeeAsync(Payee.Id);
-                if (result.Success)
-                {
-                    _needToSync = true;
+                IsBusy = true;
 
-                    if (Device.RuntimePlatform == Device.macOS)
+                try
+                {
+                    BusyText = _resourceContainer.GetResourceString("BusyTextDeleting");
+                    var result = await _payeeLogic.SoftDeletePayeeAsync(Payee.Id);
+                    if (result.Success)
                     {
-                        await _navigationService.GoBackAsync();
+                        _needToSync = true;
+
+                        if (Device.RuntimePlatform == Device.macOS)
+                        {
+                            await _navigationService.GoBackAsync();
+                        }
+                        else
+                        {
+                            await _navigationService.GoBackToRootAsync();
+                        }
                     }
                     else
                     {
-                        await _navigationService.GoBackToRootAsync();
+                        await _dialogService.DisplayAlertAsync(_resourceContainer.GetResourceString("AlertDeleteUnsuccessful"), result.Message, _resourceContainer.GetResourceString("AlertOk"));
                     }
                 }
-                else
+                finally
                 {
-                    await _dialogService.DisplayAlertAsync(_resourceContainer.GetResourceString("AlertDeleteUnsuccessful"), result.Message, _resourceContainer.GetResourceString("AlertOk"));
+                    IsBusy = false;
                 }
-            }
-            finally
-            {
-                IsBusy = false;
             }
         }
 
