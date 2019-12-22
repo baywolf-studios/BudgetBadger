2999593ec36cc74d61b0f766674c6fbea032ada5
Matthew Pritchett <matthewpritchett@Pritchett-iMac.local>
2019-12-22 10:19:30 -0600

    Initial testing of font icons

---
+++ BudgetBadger.Forms/Style/FontIconResourceDictionary.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BudgetBadger.Forms.Style
+{
+    public partial class FontIconResourceDictionary : ResourceDictionary
+    {
+        public FontIconResourceDictionary()
+        {
+            InitializeComponent();
+        }
+    }
+}
