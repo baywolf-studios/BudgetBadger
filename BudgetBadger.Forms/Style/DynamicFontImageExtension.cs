0755cf2e62ed1a0c57ef7b9f79188f63f2ca007d
Matthew Pritchett <matthewpritchett@Pritchett-iMac.local>
2020-01-01 10:54:03 -0600

    testing more stuff out and removing more svgs

---
+++ BudgetBadger.Forms/Style/DynamicFontImageExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BudgetBadger.Forms.Style
+{
+    [AcceptEmptyServiceProvider]
+    [ContentProperty(nameof(GlyphKey))]
+    public class DynamicFontImageExtension : IMarkupExtension<ImageSource>
+    {
+        public string FontFamilyKey { get; set; }
+        public string GlyphKey { get; set; }
+        public string ColorKey { get; set; }
+        public string SizeKey { get; set; }
+
+        public ImageSource ProvideValue(IServiceProvider serviceProvider)
+        {
+            var fontImageSource = new FontImageSource();
+            fontImageSource.SetDynamicResource(FontImageSource.FontFamilyProperty, FontFamilyKey);
+            fontImageSource.SetDynamicResource(FontImageSource.GlyphProperty, GlyphKey);
+            fontImageSource.SetDynamicResource(FontImageSource.ColorProperty, ColorKey);
+            fontImageSource.SetDynamicResource(FontImageSource.SizeProperty, SizeKey);
+            return fontImageSource;
+        }
+
+        object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+        {
+            return (this as IMarkupExtension<ImageSource>).ProvideValue(serviceProvider);
+        }
+    }
+}
