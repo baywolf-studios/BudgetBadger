38898a9744a897e741b99196341a8e973f7fe9d5
Matthew Pritchett <matthewpritchett@Pritchett-iMac.local>
2020-01-03 21:07:51 -0600

    fixing fontimagesource

---
+++ BudgetBadger.Forms/UserControls/ListView2.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace BudgetBadger.Forms.UserControls
+{
+    public partial class ListView2 : Xamarin.Forms.ListView
+    {
+        public ListView2()
+        {
+            InitializeComponent();
+        }
+    }
+}
