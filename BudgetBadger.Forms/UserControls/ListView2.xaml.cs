e4998f16c809f8d28358748317f9dc73b46877d4
Matthew Pritchett <matthewpritchett@Pritchett-iMac.local>
2020-01-05 11:44:07 -0600

    lots of changes

--- BudgetBadger.Forms/UserControls/ListView2.xaml.cs
+++ BudgetBadger.Forms/UserControls/ListView2.xaml.cs
@@ -1,14 +1,142 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using BudgetBadger.Models;
 using Xamarin.Forms;
 
 namespace BudgetBadger.Forms.UserControls
 {
     public partial class ListView2 : Xamarin.Forms.ListView
     {
+        readonly ObservableRangeCollection<object> internalItems;
+
+        public static BindableProperty FilterProperty = BindableProperty.Create(nameof(Filter), typeof(Predicate<object>), typeof(ListView2), propertyChanged: UpdateFilter);
+        public Predicate<object> Filter
+        {
+            get => (Predicate<object>)GetValue(FilterProperty);
+            set => SetValue(FilterProperty, value);
+        }
+
+        public static BindableProperty FilterTextProperty = BindableProperty.Create(nameof(FilterText), typeof(string), typeof(ListView2));
+        public string FilterText
+        {
+            get => (string)GetValue(FilterTextProperty);
+            set => SetValue(FilterTextProperty, value);
+        }
+
+        public static BindableProperty IsGroupedProperty = BindableProperty.Create(nameof(IsGrouped), typeof(bool), typeof(ListView2), propertyChanged: UpdateGrouped);
+        public bool IsGrouped
+        {
+            get => (bool)GetValue(IsGroupedProperty);
+            set => SetValue(IsGroupedProperty, value);
+        }
+
+        public static BindableProperty GroupPropertyDescriptionProperty = BindableProperty.Create(nameof(GroupPropertyDescription), typeof(string), typeof(ListView2));
+        public string GroupPropertyDescription
+        {
+            get => (string)GetValue(GroupPropertyDescriptionProperty);
+            set => SetValue(GroupPropertyDescriptionProperty, value);
+        }
+
+        public static BindableProperty IsSortedProperty = BindableProperty.Create(nameof(IsSorted), typeof(bool), typeof(ListView2), propertyChanged: UpdateSorted);
+        public bool IsSorted
+        {
+            get => (bool)GetValue(IsSortedProperty);
+            set => SetValue(IsSortedProperty, value);
+        }
+
+        public static BindableProperty SortComparerProperty = BindableProperty.Create(nameof(SortComparer), typeof(IComparer<Object>), typeof(ListView2));
+        public IComparer<Object> SortComparer
+        {
+            get => (IComparer<Object>)GetValue(SortComparerProperty);
+            set => SetValue(SortComparerProperty, value);
+        }
+
         public ListView2()
         {
             InitializeComponent();
+            internalItems = new ObservableRangeCollection<object>();
+        }
+
+        private static void UpdateFilter(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (bindable is ListView2 listView)
+            {
+                listView.internalItems.Clear();
+                listView.UpdateItems();
+            }
+        }
+
+        private static void UpdateGrouped(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (bindable is ListView2 listView)
+            {
+                listView.internalItems.Clear();
+                listView.UpdateItems();
+            }
+        }
+
+        private static void UpdateSorted(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (bindable is ListView2 listView)
+            {
+                listView.internalItems.Clear();
+                listView.UpdateItems();
+            }
+        }
+
+        private void UpdateItems()
+        {
+            var filteredItems = new List<object>();
+
+            // filtering
+            if (Filter != null)
+            {
+                foreach (var item in internalItems)
+                {
+                    if (Filter.Invoke(item))
+                    {
+                        filteredItems.Add(item);
+                    }
+                }
+            }
+            else
+            {
+                filteredItems.AddRange(internalItems);
+            }
+
+            //grouping
+            if (IsGrouped)
+            {
+                var groupedItems = filteredItems.GroupBy(g => GetPropertyValue(g, GroupPropertyDescription));
+
+                //merge each groupedItems into the internalList grouped item
+
+                //add any new groupedItems that don't already exist in the internalList
+
+                //remove any groups in the internalList if they don't exist in the groupedItems
+
+                foreach (var group in groupedItems)
+                {
+                    //if (internalItems.Contains.)
+                }
+            }
+
+            if (IsSorted)
+            {
+                internalItems.Sort(SortComparer);
+            }
+            else
+            {
+
+            }
+        }
+
+        object GetPropertyValue(object src, string propertyName)
+        {
+            return src.GetType().GetRuntimeProperty(propertyName)?.GetValue(src);
         }
     }
 }
--- BudgetBadger.Forms/Views/MainDesktopPage.xaml.cs
+++ BudgetBadger.Forms/Views/MainDesktopPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using BudgetBadger.Core.Settings;
 using BudgetBadger.Forms.Enums;
 using BudgetBadger.Forms.Envelopes;
+using BudgetBadger.Forms.Style;
 using BudgetBadger.Forms.UserControls;
 using FFImageLoading.Svg.Forms;
 using Prism.Ioc;
@@ -19,46 +20,66 @@ namespace BudgetBadger.Forms.Views
             if (pageName != "EnvelopesPage")
             {
                 EnvelopesFrame.BackgroundColor = EnvelopesFrame.RestingBackgroundColor;
+                EnvelopesLabel.TextColor = (Color)DynamicResourceProvider.Instance["gray_700"];
+                EnvelopesIconFont.Color = (Color)DynamicResourceProvider.Instance["gray_700"];
             }
             else
             {
                 EnvelopesFrame.BackgroundColor = EnvelopesFrame.ActiveBackgroundColor;
+                EnvelopesLabel.TextColor = (Color)DynamicResourceProvider.Instance["brand_700"];
+                EnvelopesIconFont.Color = (Color)DynamicResourceProvider.Instance["brand_700"];
             }
 
             if (pageName != "AccountsPage")
             {
                 AccountsFrame.BackgroundColor = AccountsFrame.RestingBackgroundColor;
+                AccountsLabel.TextColor = (Color)DynamicResourceProvider.Instance["gray_700"];
+                AccountsIconFont.Color = (Color)DynamicResourceProvider.Instance["gray_700"];
             }
             else
             {
                 AccountsFrame.BackgroundColor = AccountsFrame.ActiveBackgroundColor;
+                AccountsLabel.TextColor = (Color)DynamicResourceProvider.Instance["brand_700"];
+                AccountsIconFont.Color = (Color)DynamicResourceProvider.Instance["brand_700"];
             }
 
             if (pageName != "PayeesPage")
             {
                 PayeesFrame.BackgroundColor = PayeesFrame.RestingBackgroundColor;
+                PayeesLabel.TextColor = (Color)DynamicResourceProvider.Instance["gray_700"];
+                PayeesIconFont.Color = (Color)DynamicResourceProvider.Instance["gray_700"];
             }
             else
             {
                 PayeesFrame.BackgroundColor = PayeesFrame.ActiveBackgroundColor;
+                PayeesLabel.TextColor = (Color)DynamicResourceProvider.Instance["brand_700"];
+                PayeesIconFont.Color = (Color)DynamicResourceProvider.Instance["brand_700"];
             }
 
             if (pageName != "ReportsPage")
             {
                 ReportsFrame.BackgroundColor = ReportsFrame.RestingBackgroundColor;
+                ReportsLabel.TextColor = (Color)DynamicResourceProvider.Instance["gray_700"];
+                ReportsIconFont.Color = (Color)DynamicResourceProvider.Instance["gray_700"];
             }
             else
             {
                 ReportsFrame.BackgroundColor = ReportsFrame.ActiveBackgroundColor;
+                ReportsLabel.TextColor = (Color)DynamicResourceProvider.Instance["brand_700"];
+                ReportsIconFont.Color = (Color)DynamicResourceProvider.Instance["brand_700"];
             }
 
             if (pageName != "SettingsPage")
             {
                 SettingsFrame.BackgroundColor = SettingsFrame.RestingBackgroundColor;
+                SettingsLabel.TextColor = (Color)DynamicResourceProvider.Instance["gray_700"];
+                SettingsIconFont.Color = (Color)DynamicResourceProvider.Instance["gray_700"];
             }
             else
             {
                 SettingsFrame.BackgroundColor = SettingsFrame.ActiveBackgroundColor;
+                SettingsLabel.TextColor = (Color)DynamicResourceProvider.Instance["brand_700"];
+                SettingsIconFont.Color = (Color)DynamicResourceProvider.Instance["brand_700"];
             }
         }
 
