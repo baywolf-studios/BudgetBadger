405251dd405e5a2c7d133973d562f3bbea5398b2
Matthew Pritchett <matthewpritchett@Pritchett-iMac.local>
2020-01-11 11:42:28 -0600

    scrolling

---
+++ BudgetBadger.macOS/Renderers/ListViewRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using AppKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+[assembly: ExportRenderer(typeof(ListView), typeof(BudgetBadger.macOS.Renderers.ListViewRenderer))]
+namespace BudgetBadger.macOS.Renderers
+{
+    public class ListViewRenderer : Xamarin.Forms.Platform.MacOS.ListViewRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<ListView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null && Control != null && Control is NSScrollView scroller)
+            {
+                NSScrollView.Notifications.ObserveDidLiveScroll(scroller, (sender, args) => {
+                    if (args.Notification.Object is NSScrollView nsScrollView)
+                    {
+                        var scrolledArgs = new ScrolledEventArgs(nsScrollView.DocumentVisibleRect.X, nsScrollView.DocumentVisibleRect.Y);
+                        Element.SendScrolled(scrolledArgs);
+                    }
+                });
+            }
+        }
+    }
+}
