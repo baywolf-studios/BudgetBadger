64370ab8c2d604000ee5162e3e3f13a714268cf6
Matthew Pritchett <matthewpritchett@Pritchett-iMac.local>
2020-01-13 19:29:31 -0600

    more list and datatable work

--- BudgetBadger.Forms/DataTemplates/AccountDetailedViewCell.xaml.cs
+++ BudgetBadger.Forms/DataTemplates/AccountDetailedViewCell.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Windows.Input;
 using BudgetBadger.Forms.UserControls;
 using Xamarin.Forms;
 
@@ -7,9 +8,30 @@ namespace BudgetBadger.Forms.DataTemplates
 {
     public partial class AccountDetailedViewCell : SelectableViewCell
     {
+        public static BindableProperty SaveCommandProperty = BindableProperty.Create(nameof(SaveCommand), typeof(ICommand), typeof(TextColumn));
+        public ICommand SaveCommand
+        {
+            get => (ICommand)GetValue(SaveCommandProperty);
+            set => SetValue(SaveCommandProperty, value);
+        }
+
         public AccountDetailedViewCell()
         {
             InitializeComponent();
+            DescriptionControl.Completed += DescriptionControl_Completed;
+        }
+
+        private void DescriptionControl_Completed(object sender, EventArgs e)
+        {
+            if (SaveCommand != null && SaveCommand.CanExecute(BindingContext))
+            {
+                SaveCommand.Execute(BindingContext);
+            }
+        }
+
+        private void HandleTapped(object sender, EventArgs e)
+        {
+            DescriptionControl.Focus();
         }
     }
 }
