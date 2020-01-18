a2de165c3a7a9c1d16687023a4b7d8c28fbccb18
Matthew Pritchett <matthewpritchett@Pritchett-iMac.local>
2020-01-18 10:40:49 -0600

    beginning of trnasaction

---
+++ BudgetBadger.Forms/DataTemplates/TransactionDetailedHeader.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace BudgetBadger.Forms.DataTemplates
+{
+    public partial class TransactionDetailedHeader : Grid
+    {
+        public TransactionDetailedHeader()
+        {
+            InitializeComponent();
+        }
+    }
+}
