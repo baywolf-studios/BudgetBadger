5da472742241d7ecd95d40319082214dcb871cac
Matthew Pritchett <matthewpritchett@Pritchett-iMac.local>
2020-01-21 18:57:23 -0600

    more ui

--- BudgetBadger.Forms/UserControls/ResponsiveGrid.xaml.cs
+++ BudgetBadger.Forms/UserControls/ResponsiveGrid.xaml.cs
@@ -6,49 +6,49 @@ namespace BudgetBadger.Forms.UserControls
 {
     public partial class ResponsiveGrid : Grid
     {
+        public static BindableProperty GutterProperty =
+            BindableProperty.Create(nameof(Gutter),
+                typeof(double),
+                typeof(ResponsiveGrid),
+                defaultBindingMode: BindingMode.OneWayToSource);
+        public double Gutter
+        {
+            get => (double)GetValue(GutterProperty);
+            set => SetValue(GutterProperty, value);
+        }
+
         public ResponsiveGrid()
         {
             InitializeComponent();
-            SizeChanged += ResponsiveGrid_SizeChanged;
-        }
-
-        private void ResponsiveGrid_SizeChanged(object sender, EventArgs e)
-        {
-            if (Width > 0 && Device.RuntimePlatform != Device.macOS)
-            {
-                if (Width <= 304)
-                {
-                    Padding = new Thickness(16,0,16,0);
-                    ColumnSpacing = 16;
-                }
-                else if (Width <= 768)
-                {
-                    Padding = new Thickness(24, 0, 24, 0);
-                    ColumnSpacing = 24;
-                }
-                else if (Width <= 1280)
-                {
-                    Padding = new Thickness(32, 0, 32, 0);
-                    ColumnSpacing = 32;
-                }
-                else if (Width <= 1768)
-                {
-                    Padding = new Thickness(40, 0, 40, 0);
-                    ColumnSpacing = 40;
-                }
-                else
-                {
-                    Padding = new Thickness(48, 0, 48, 0);
-                    ColumnSpacing = 48;
-                }
-            }
         }
 
         protected override void OnSizeAllocated(double width, double height)
         {
             base.OnSizeAllocated(width, height);
 
-            
+            if (Width > 0)
+            {
+                if (Width <= 768)
+                {
+                    Gutter = 16;
+                }
+                else if (Width <= 1280)
+                {
+                    Gutter = 24;
+                }
+                else if (Width <= 1768)
+                {
+                    Gutter = 32;
+                }
+                else if (Width <= 2160)
+                {
+                    Gutter = 40;
+                }
+                else
+                {
+                    Gutter = 48;
+                }
+            }
         }
     }
 }
