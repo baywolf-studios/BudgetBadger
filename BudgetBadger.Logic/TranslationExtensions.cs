0c753ba00caa426ab6c9cb06e38782c654dc4123
Matthew Pritchett <matthewpritchett@Pritchett-iMac.local>
2020-05-09 15:39:33 -0500

    Create TranslationExtensions.cs

---
+++ BudgetBadger.Logic/TranslationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BudgetBadger.Core.LocalizedResources;
+using BudgetBadger.Models;
+
+namespace BudgetBadger.Logic
+{
+    public static class TranslationExtensions
+    {
+        public static void TranslateEnvelope(this Envelope envelope, IResourceContainer resourceContainer)
+		{
+		}
+    }
+}
