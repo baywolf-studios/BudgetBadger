41a657f127ca724b9973731beacc1b498a358d4a
baywolf_studios <34951520+baywolf-studios@users.noreply.github.com>
2020-12-30 20:43:54 -0600

    fixed search bar background in app bar for dark mode, fixed text color changing in labels on mac

---
+++ BudgetBadger.macOS/Renderers/LabelRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using BudgetBadger.macOS.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+[assembly: ExportRenderer(typeof(Label), typeof(MacLabelRenderer))]
+namespace BudgetBadger.macOS.Renderers
+{
+    public class MacLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == Label.TextProperty.PropertyName)
+            {
+                Control.TextColor = Element.TextColor.ToNSColor();
+            }
+
+            base.OnElementPropertyChanged(sender, e);
+        }
+    }
+}
