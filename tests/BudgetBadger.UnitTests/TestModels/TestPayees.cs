457a5147a4700d8228272017be6e898c810388a9
Matthew Pritchett <34951520+matthewpritchett@users.noreply.github.com>
2022-04-26 10:34:17 -0500

    chore: Setup Github Actions Workflows

---
+++ tests/BudgetBadger.UnitTests/TestModels/TestPayees.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using BudgetBadger.Models;
+
+namespace BudgetBadger.UnitTests.TestModels
+{
+    public static class TestPayees
+    {
+        public static readonly Payee NewPayee = new Payee
+        {
+            Description = nameof(NewPayee)
+        };
+
+        public static readonly Payee ActivePayee = new Payee
+        {
+            Id = Guid.NewGuid(),
+            Description = nameof(ActivePayee),
+            CreatedDateTime = DateTime.Now,
+            ModifiedDateTime = DateTime.Now
+        };
+
+        public static readonly Payee HiddenPayee = new Payee
+        {
+            Id = Guid.NewGuid(),
+            Description = nameof(HiddenPayee),
+            CreatedDateTime = DateTime.Now,
+            ModifiedDateTime = DateTime.Now,
+            HiddenDateTime = DateTime.Now
+        };
+
+        public static readonly Payee SoftDeletedPayee = new Payee
+        {
+            Id = Guid.NewGuid(),
+            Description = nameof(SoftDeletedPayee),
+            CreatedDateTime = DateTime.Now,
+            ModifiedDateTime = DateTime.Now,
+            HiddenDateTime = DateTime.Now,
+            DeletedDateTime = DateTime.Now
+        };
+    }
+}
