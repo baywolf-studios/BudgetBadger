db441eb1778b3caddfcdabfc5040fddb7f176328
Matthew Pritchett <34951520+matthewpritchett@users.noreply.github.com>
2023-02-21 06:30:36 -0600

    build: Run tests on every commit, builds on PRs, release on commits to Main.

---
+++ tests/BudgetBadger.IntegrationTests/IntegrationTestSecrets.cs
@@ -0,0 +1,12 @@
+namespace BudgetBadger.IntegrationTests;
+
+public static class IntegrationTestSecrets
+{
+    public static string DropBoxRefreshToken => "";
+    public static string DropBoxAppKey => "";
+    public static string DropBoxAppSecret => "";
+    public static string WebDavServer => "";
+    public static string WebDavDirectory => "";
+    public static string WebDavUsername => "";
+    public static string WebDavPassword => "";
+}
--- tests/BudgetBadger.IntegrationTests/IntegrationTestSecrets.example.cs
+++
@@ -1,8 +0,0 @@
-// namespace BudgetBadger.IntegrationTests;
-//
-// public static class IntegrationTestSecrets
-// { 
-//     public static string DropBoxRefreshToken => "";
-//     public static string DropBoxAppKey => "";
-//     public static string DropBoxAppSecret => "";
-// }
